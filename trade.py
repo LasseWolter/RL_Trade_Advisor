8e8a83bd8928b34755b1e56e7efe2825fb2cc1b6
Lasse <lasse@go4more.de>
2018-06-14 16:27:29 +0100

    #3 oneToOne offers + added list containing the offers one-to-one + added list conaining the relevant offers of oneToOne-list + separated getItems() from getOffers() + added printDict() function to neatly print dictionaries

--- trade.py
+++ trade.py
@@ -1,5 +1,12 @@
 from lxml import html
 import requests
+from enum import Enum
+
+# Enumeration for the side of the offer
+class Side(Enum):
+    HAS = 1
+    WANTS = 2
+
 
 # Round floating point number to 2 digits
 def roundTo2Dig(num):
@@ -26,40 +33,66 @@ def getPrices():
         prices[element[0]] = av_price
     return prices
 
+# Get items from particular side for particular offer
+def getItems(offer, side):
+    items=[]
+
+    if side == Side.HAS:
+        item_paths = offer.xpath('.//*[@id="rlg-youritems"]//*[@class="rlg-trade-display-item rlg-trade-display-item-read"]')
+    elif side == Side.WANTS:
+        item_paths = offer.xpath('.//*[@id="rlg-theiritems"]//*[@class="rlg-trade-display-item rlg-trade-display-item-read"]')    
+    else:
+        raise ValueError("Invalid offer side")
+
+    for i in item_paths:
+        name = i.xpath('.//img/@alt')
+        if not name == []:
+            name = name[0]
+        else:
+            name = ''
+        num_str = i.xpath('.//*[contains(@class,"rlg-trade-display-item__amount")]/text()')     # contains has to be used since the class contains the rarity of the item ("rare, "premium",etc)
+        if (num_str == []): # if the amount is not given it's 1 item
+            num = 1
+        else: 
+            num = int(num_str[0]) # index 0 because num_str is a list of one string
+
+        item = (name, num)
+        items.append(item)
+    return items
+
 def getOffers():
     base_url = "https://rocket-league.com/trading?filterItem=1159&filterCertification=0&filterPaint=0&filterPlatform=2&filterSearchType=2&p="
     offs = {}
     # Just one page right now
-    max_page = 1
+    max_page = 9
 
+    # iterate over all pages
     for i in range (0,max_page):
         trade_url = base_url + str(i)
         # Get Items you can get for triumph crate
         trade_html = requests.get(trade_url)
         trade_tree = html.fromstring(trade_html.content)
 
-        items = trade_tree.xpath('//*[@id="rlg-youritems"]//img/@alt')
-
-        n = 1
         offers = trade_tree.xpath('//*[@class="rlg-trade-display-container is--user"]')
         for o in offers:
-            # get offer link
+            # Get offer link
             link = o.xpath('./div[@class="rlg-trade-display-header"]/a/@href')
             link_str = "https://rocket-league.com/" + link[0]
-            # get offer items 
-            # items_path = o.xpath('.//*[@id="rlg-youritems"]')
-            item_paths = o.xpath('.//*[@id="rlg-youritems"]//*[@class="rlg-trade-display-item rlg-trade-display-item-read"]')
-            items=[]
-            for i in item_paths:
-                name = i.xpath('.//img/@alt')
-                num_strs = i.xpath('.//*[@class="rlg-trade-display-item__amount is--premium"]/text()')
-                if (num_strs == []): # if the amount is not given it's 1 item
-                    num = 1
-                else: 
-                    num = int(num_strs[0]) # index 0 because num_strs a list of one string
-                # print link_str
-                item = (name, num)
-                items.append(item)
-            
-            offs[link_str] = items
-        return offs
+
+            # Get items on 'Has'-side in offer o  
+            itemsHas = getItems(o, Side.HAS)
+            itemsWants = getItems(o, Side.WANTS)
+            oneToOne = []
+            relOffers = []
+            if len(itemsHas) == len(itemsWants):
+                oneToOne = zip(itemsHas, itemsWants)
+                for off in oneToOne:
+                    if off[1][0] == "Triumph Crate":
+                        relOffers.append(off)
+            if not relOffers == []:
+                offs[link_str] = relOffers
+    return offs
+
+def printDict(d):
+    for link in d:
+        print link, d[link] 
